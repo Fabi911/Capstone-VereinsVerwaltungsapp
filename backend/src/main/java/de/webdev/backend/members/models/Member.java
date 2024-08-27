8e1d1a07d342c52d0ff686f2b3041b6d698a81e5
Fabian Döz <fabian@doez.info>
2024-08-27 11:50:33 +0200

    little fix

--- backend/src/main/java/de/webdev/backend/members/models/Member.java
+++ backend/src/main/java/de/webdev/backend/members/models/Member.java
@@ -1,6 +1,10 @@
 package de.webdev.backend.members.models;
 
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+
 public record Member(
+        @MongoId
         String id,
         String lastName,
         String name,
