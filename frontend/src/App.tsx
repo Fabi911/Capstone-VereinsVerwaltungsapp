0b0b06ea65f2c1fc5a80b34cd2e2addae0f010e6
Fabian Döz <fabian@doez.info>
2024-08-27 17:25:19 +0200

    Add layout and routing with styled components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,17 @@
 
+import { Routes, Route } from 'react-router-dom';
+import Dashboard from "./components/Pages/Dashboard.tsx";
+import Layout from "./components/Layout/Layout.tsx";
+
+
 function App() {
-
-
-  return (
-    <>
-        <h1>Hello World</h1>
-    </>
-  )
+    return (
+            <Layout>
+                <Routes>
+                    <Route path="/" element={<Dashboard/>}/>
+                </Routes>
+            </Layout>
+    )
 }
 
 export default App
