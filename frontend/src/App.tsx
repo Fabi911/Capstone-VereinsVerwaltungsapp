fbb6c31db25310cab4a2acada732e8f5089f609c
Fabian Döz <fabian@doez.info>
2024-08-28 15:08:22 +0200

    add Form to add new Members in a Modal

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,33 +2,18 @@
 import { Routes, Route } from 'react-router-dom';
 import Dashboard from "./Pages/Dashboard.tsx";
 import Layout from "./components/Layout/Layout.tsx";
-import {useEffect, useState} from "react";
-import axios from "axios";
 import MembersOverview from "./Pages/MembersOverview.tsx";
 
 
 function App() {
-   const [membersDB, setMembersDB] = useState([]);
 
-   function fetchMembers():void {
-        axios.get('api/members')
-            .then(response => {
-                setMembersDB(response.data);
-            })
-            .catch(error => {
-                console.log(error);
-   })}
-
-    useEffect(() => {
-        fetchMembers();
-    }, []);
 
 
     return (
             <Layout>
                 <Routes>
                     <Route path="/" element={<Dashboard/>}/>
-                    <Route path="/members" element={<MembersOverview members={membersDB}/>}/>
+                    <Route path="/members" element={<MembersOverview/>}/>
                 </Routes>
             </Layout>
     )
