6481a2371b268345d9e28ec1e765f8363848ba9c
Fabian Döz <fabian@doez.info>
2024-09-06 20:00:50 +0200

    Add Spring Security dependencies and secure error controller
    Added `spring-security-test` and `spring-security-core` dependencies to the `pom.xml`. Secured the `CustomErrorController` by annotating the error-handling method with `@Secured("ROLE_USER")`.

--- backend/src/main/java/de/webdev/backend/members/controller/CustomErrorController.java
+++ backend/src/main/java/de/webdev/backend/members/controller/CustomErrorController.java
@@ -2,6 +2,7 @@ package de.webdev.backend.members.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequiredArgsConstructor
 public class CustomErrorController implements ErrorController {
 
+	@Secured("ROLE_USER")
 	@RequestMapping(value = "/error", method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 	public String handleError() {
 		return "redirect:/";
