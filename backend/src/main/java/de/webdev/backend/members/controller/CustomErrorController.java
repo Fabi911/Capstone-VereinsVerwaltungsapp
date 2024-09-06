c854822b2ef073a661530add15e812215f3e31c1
Fabian Döz <fabian@doez.info>
2024-09-06 19:53:09 +0200

    Add HTTP methods to error handling
    Include GET, POST, PUT, and DELETE HTTP methods in the error handling request mapping. Also, introduce the `RequiredArgsConstructor` annotation to the `CustomErrorController` class.

--- backend/src/main/java/de/webdev/backend/members/controller/CustomErrorController.java
+++ backend/src/main/java/de/webdev/backend/members/controller/CustomErrorController.java
@@ -1,14 +1,17 @@
 package de.webdev.backend.members.controller;
 
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 
 @Controller
+@RequiredArgsConstructor
 public class CustomErrorController implements ErrorController {
 
-	@RequestMapping("/error")
+	@RequestMapping(value = "/error", method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 	public String handleError() {
 		return "redirect:/";
 	}
