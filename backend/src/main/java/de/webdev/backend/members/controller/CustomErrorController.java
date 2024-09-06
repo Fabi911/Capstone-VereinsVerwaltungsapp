a8ff7033e38406b69aaa921c64fe3618bb232732
Fabian Döz <fabian@doez.info>
2024-09-06 20:10:03 +0200

    sch... SonarCloud befriedigen

--- backend/src/main/java/de/webdev/backend/members/controller/CustomErrorController.java
+++ backend/src/main/java/de/webdev/backend/members/controller/CustomErrorController.java
@@ -2,18 +2,20 @@ package de.webdev.backend.members.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.security.access.annotation.Secured;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
 
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping
 public class CustomErrorController implements ErrorController {
 
-	@Secured("ROLE_USER")
-	@RequestMapping(value = "/error", method = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
+
+	@GetMapping("/error")
 	public String handleError() {
 		return "redirect:/";
 	}
