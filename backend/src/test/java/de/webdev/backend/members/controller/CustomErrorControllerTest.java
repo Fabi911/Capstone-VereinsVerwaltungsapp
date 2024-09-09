3fe7b88863ed731ead962c432b2e243e761f2879
Fabian Döz <fabian@doez.info>
2024-09-09 15:39:56 +0200

    fix tests

--- backend/src/test/java/de/webdev/backend/members/controller/CustomErrorControllerTest.java
+++ backend/src/test/java/de/webdev/backend/members/controller/CustomErrorControllerTest.java
@@ -19,7 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
     private MockMvc mockMvc;
 
     @Test
-    @WithMockUser
+    @WithMockUser(roles="USER")
     void handleError_redirectsToRoot() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/error"))
                 .andExpect(status().is3xxRedirection())
