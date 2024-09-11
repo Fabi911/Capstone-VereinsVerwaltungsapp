b3e93fee48d5fb7b01b8f9671fdd826a33374b8c
Fabian Döz <fabian@doez.info>
2024-09-11 09:34:47 +0200

    add new tests for Users
    Corrected inconsistent indentation in MemberControllerTest. Added test cases to AppuserServiceTest for finding all users, updating roles, and deleting users. Also added new controller tests in AppuserControllerTest.

--- backend/src/test/java/de/webdev/backend/members/controller/MemberControllerTest.java
+++ backend/src/test/java/de/webdev/backend/members/controller/MemberControllerTest.java
@@ -17,158 +17,159 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 @SpringBootTest
 class MemberControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+	@Autowired
+	private MockMvc mockMvc;
 
 
-    @DirtiesContext
-    @Test
-    @WithMockUser(roles="ADMIN")
-    void getMembers_shouldReturnEmptyList_whenCallInitially() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/members")).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.content().json("[]"));
-    }
+	@DirtiesContext
+	@Test
+	@WithMockUser(roles = "ADMIN")
+	void getMembers_shouldReturnEmptyList_whenCallInitially() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/members")).andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().json("[]"));
+	}
 
-    @DirtiesContext
-    @Test
-    @WithMockUser(roles="ADMIN")
-    void addMember_shouldReturnMember_whenAddMember() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/members").contentType("application/json").content("""
-                 {
-                         "memberId": "2024-002",
-                         "lastName": "Muster",
-                         "name": "Fabian",
-                         "birthday": "1988-02-16",
-                         "address": {
-                             "street": "Musterstraße 4",
-                             "zip": "12345",
-                             "city": "Musterstadt"
-                         },
-                            "email": "test@tester.com",
-                            "phoneNumber": "123456789"
-                  }
-                """)).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.content().json("""
-                                {
-                "memberId": "2024-002",
-                "lastName": "Muster",
-                "name": "Fabian",
-                "birthday": "1988-02-16",
-                "address": {
-                    "street": "Musterstraße 4",
-                    "zip": "12345",
-                    "city": "Musterstadt"
-                },
-                    "email": "test@tester.com",
-                    "phoneNumber": "123456789"
-                }
-                """));
-    }
+	@DirtiesContext
+	@Test
+	@WithMockUser(roles = "ADMIN")
+	void addMember_shouldReturnMember_whenAddMember() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/members").contentType("application/json").content("""
+				 {
+				         "memberId": "2024-002",
+				         "lastName": "Muster",
+				         "name": "Fabian",
+				         "birthday": "1988-02-16",
+				         "address": {
+				             "street": "Musterstraße 4",
+				             "zip": "12345",
+				             "city": "Musterstadt"
+				         },
+				            "email": "test@tester.com",
+				            "phoneNumber": "123456789"
+				  }
+				""")).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.content().json("""
+				                {
+				"memberId": "2024-002",
+				"lastName": "Muster",
+				"name": "Fabian",
+				"birthday": "1988-02-16",
+				"address": {
+				    "street": "Musterstraße 4",
+				    "zip": "12345",
+				    "city": "Musterstadt"
+				},
+				    "email": "test@tester.com",
+				    "phoneNumber": "123456789"
+				}
+				"""));
+	}
 
-    @DirtiesContext
-    @Test
-    @WithMockUser(roles="ADMIN")
-    void getMemberById_shouldReturnMember_whenGetMemberById() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/members").contentType("application/json").content("""
-                {
-                        "memberId": "2024-002",
-                        "lastName": "Muster",
-                        "name": "Fabian",
-                        "birthday": "1988-02-16",
-                        "address": {
-                            "street": "Musterstraße 4",
-                            "zip": "12345",
-                            "city": "Musterstadt"
-                        },
-                           "email": "test@tester.com",
-                           "phoneNumber": "123456789"
-                           }
-                """));
-        mockMvc.perform(MockMvcRequestBuilders.get("/api/members/2024-002").contentType("application/json")).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.content().json("""
-                {
-                "memberId": "2024-002",
-                "lastName": "Muster",
-                "name": "Fabian",
-                "birthday": "1988-02-16",
-                "address": {
-                    "street": "Musterstraße 4",
-                    "zip": "12345",
-                    "city": "Musterstadt"
-                },
-                    "email": "test@tester.com",
-                      "phoneNumber": "123456789"
-                      }
-                """));
+	@DirtiesContext
+	@Test
+	@WithMockUser(roles = "ADMIN")
+	void getMemberById_shouldReturnMember_whenGetMemberById() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/members").contentType("application/json").content("""
+				{
+				        "memberId": "2024-002",
+				        "lastName": "Muster",
+				        "name": "Fabian",
+				        "birthday": "1988-02-16",
+				        "address": {
+				            "street": "Musterstraße 4",
+				            "zip": "12345",
+				            "city": "Musterstadt"
+				        },
+				           "email": "test@tester.com",
+				           "phoneNumber": "123456789"
+				           }
+				"""));
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/members/2024-002").contentType("application/json")).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.content().json("""
+				{
+				"memberId": "2024-002",
+				"lastName": "Muster",
+				"name": "Fabian",
+				"birthday": "1988-02-16",
+				"address": {
+				    "street": "Musterstraße 4",
+				    "zip": "12345",
+				    "city": "Musterstadt"
+				},
+				    "email": "test@tester.com",
+				      "phoneNumber": "123456789"
+				      }
+				"""));
 
-    }
+	}
 
-    @DirtiesContext
-    @Test
-    @WithMockUser(roles="ADMIN")
-    void updateMember_shouldReturnUpdatedMember_whenUpdateMember() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/members").contentType("application/json").content("""
-                {
-                        "memberId": "2024-002",
-                        "lastName": "Muster",
-                        "name": "Fabian",
-                        "birthday": "1988-02-16",
-                        "address": {
-                            "street": "Musterstraße 4",
-                            "zip": "12345",
-                            "city": "Musterstadt"
-                        },
-                        "email": "test@tester.com",
-                        "phoneNumber": "123456789"
-                     }
-                """));
-        mockMvc.perform(MockMvcRequestBuilders.put("/api/members/2024-002").contentType("application/json").content("""
-                {
-                        "memberId": "2024-002",
-                        "lastName": "Mustermann",
-                        "name": "Erika",
-                        "birthday": "1985-03-12",
-                        "address": {
-                            "street": "Musterstraße 5",
-                            "zip": "12346",
-                            "city": "Musterstadt"
-                        },
-                        "email": "test2@tester.com",
-                        "phoneNumber": "987654321"
-                     }
-                """)).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.content().json("""
-                    {
-                        "memberId": "2024-002",
-                        "lastName": "Mustermann",
-                        "name": "Erika",
-                        "birthday": "1985-03-12",
-                        "address": {
-                            "street": "Musterstraße 5",
-                            "zip": "12346",
-                            "city": "Musterstadt"
-                        },
-                        "email": "test2@tester.com",
-                        "phoneNumber": "987654321"
-                    }
-                """));
-    }
+	@DirtiesContext
+	@Test
+	@WithMockUser(roles = "ADMIN")
+	void updateMember_shouldReturnUpdatedMember_whenUpdateMember() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/members").contentType("application/json").content("""
+				{
+				        "memberId": "2024-002",
+				        "lastName": "Muster",
+				        "name": "Fabian",
+				        "birthday": "1988-02-16",
+				        "address": {
+				            "street": "Musterstraße 4",
+				            "zip": "12345",
+				            "city": "Musterstadt"
+				        },
+				        "email": "test@tester.com",
+				        "phoneNumber": "123456789"
+				     }
+				"""));
+		mockMvc.perform(MockMvcRequestBuilders.put("/api/members/2024-002").contentType("application/json").content("""
+				{
+				        "memberId": "2024-002",
+				        "lastName": "Mustermann",
+				        "name": "Erika",
+				        "birthday": "1985-03-12",
+				        "address": {
+				            "street": "Musterstraße 5",
+				            "zip": "12346",
+				            "city": "Musterstadt"
+				        },
+				        "email": "test2@tester.com",
+				        "phoneNumber": "987654321"
+				     }
+				""")).andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.content().json("""
+				    {
+				        "memberId": "2024-002",
+				        "lastName": "Mustermann",
+				        "name": "Erika",
+				        "birthday": "1985-03-12",
+				        "address": {
+				            "street": "Musterstraße 5",
+				            "zip": "12346",
+				            "city": "Musterstadt"
+				        },
+				        "email": "test2@tester.com",
+				        "phoneNumber": "987654321"
+				    }
+				"""));
+	}
 
-    @DirtiesContext
-    @Test
-    @WithMockUser(roles="ADMIN")
-    void deleteMember_shouldReturnHttpNoContent_whenDeleteExistingMember() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/members").contentType("application/json").content("""
-                {
-                        "memberId": "2024-003",
-                        "lastName": "Muster",
-                        "name": "Fabian",
-                        "birthday": "1988-02-16",
-                        "address": {
-                            "street": "Musterstraße 4",
-                            "zip": "12345",
-                            "city": "Musterstadt"
-                        },
-                        "email": "test@tester.com",
-                        "phoneNumber": "123456789"
-                    }
-                """));
-        mockMvc.perform(MockMvcRequestBuilders.delete("/api/members/2024-003").contentType("application/json")).andExpect(MockMvcResultMatchers.status().isOk());
-    }
+	@DirtiesContext
+	@Test
+	@WithMockUser(roles = "ADMIN")
+	void deleteMember_shouldReturnHttpNoContent_whenDeleteExistingMember() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/members").contentType("application/json").content("""
+				{
+				        "memberId": "2024-003",
+				        "lastName": "Muster",
+				        "name": "Fabian",
+				        "birthday": "1988-02-16",
+				        "address": {
+				            "street": "Musterstraße 4",
+				            "zip": "12345",
+				            "city": "Musterstadt"
+				        },
+				        "email": "test@tester.com",
+				        "phoneNumber": "123456789"
+				    }
+				"""));
+		mockMvc.perform(MockMvcRequestBuilders.delete("/api/members/2024-003").contentType("application/json")).andExpect(MockMvcResultMatchers.status().isOk());
+	}
 }
