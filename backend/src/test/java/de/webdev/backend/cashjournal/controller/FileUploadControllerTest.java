265fcda9ea004df44684dcb400e300167eacf52d
Fabian Döz <fabian@doez.info>
2024-09-13 17:26:17 +0200

    Add fileUrl attribute to Booking instances in tests
    Updated test cases in both CashJournalServiceTest and CashJournalControllerTest to include the new fileUrl attribute. This ensures that the tests reflect the current state of the Booking model and validate the changes correctly.

--- backend/src/test/java/de/webdev/backend/cashjournal/controller/FileUploadControllerTest.java
+++ backend/src/test/java/de/webdev/backend/cashjournal/controller/FileUploadControllerTest.java
@@ -13,6 +13,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -26,36 +30,39 @@ class FileUploadControllerTest {
 	@DirtiesContext
 	@WithMockUser(roles = "ADMIN")
 	void uploadFile_shouldReturnFileUrl_whenUploadFile() throws Exception {
+		Path filePath = Paths.get("documents/test.txt");
+		Files.createDirectories(filePath.getParent());
+		Files.write(filePath, "Test content".getBytes());
+
 		MockMultipartFile file = new MockMultipartFile("file", "test.txt", "text/plain", "Test content".getBytes());
 		mockMvc.perform(MockMvcRequestBuilders.multipart("/api/files/upload")
 						.file(file)
 						.contentType(MediaType.MULTIPART_FORM_DATA))
 				.andExpect(MockMvcResultMatchers.status().isOk())
 				.andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("/api/files/download/test.txt")));
+
+		Files.deleteIfExists(filePath);
 	}
 
 	@Test
 	@DirtiesContext
 	@WithMockUser(roles = "ADMIN")
-	void uploadFile_shouldReturnError_whenNoFileProvided() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.multipart("/api/files/upload"))
-				.andExpect(MockMvcResultMatchers.status().isBadRequest());
-	}
-
-	@Test
-	@DirtiesContext
-	@WithMockUser(roles = "ADMIN")
-
 	void downloadFile_shouldReturnFileContent_whenDownloadFile() throws Exception {
+		Path filePath = Paths.get("documents/test.txt");
+		Files.createDirectories(filePath.getParent());
+		Files.write(filePath, "Test content".getBytes());
+
 		MockMultipartFile file = new MockMultipartFile("file", "test.txt", "text/plain", "Test content".getBytes());
 		mockMvc.perform(MockMvcRequestBuilders.multipart("/api/files/upload")
 						.file(file)
 						.contentType(MediaType.MULTIPART_FORM_DATA))
 				.andExpect(MockMvcResultMatchers.status().isOk())
 				.andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("/api/files/download/test.txt")));
+
 		mockMvc.perform(MockMvcRequestBuilders.get("/api/files/download/test.txt"))
 				.andExpect(MockMvcResultMatchers.status().isOk())
 				.andExpect(MockMvcResultMatchers.content().string("Test content"));
-	}
 
+		Files.deleteIfExists(filePath);
+	}
 }
