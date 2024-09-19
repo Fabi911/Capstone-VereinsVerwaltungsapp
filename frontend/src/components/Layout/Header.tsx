f7d2ad50b916be45cd1ad25d885f63749b204e8b
Fabian Döz <fabian@doez.info>
2024-09-19 15:52:41 +0200

    Refactor CSS and improve booking table navigation
    Refactor `styles.ts` for improved CSS structure and styling consistency. Integrated `useNavigate` hook in `BookingTable.tsx` for better navigation handling. Updated package-lock.json with new dependencies.

--- frontend/src/components/Layout/Header.tsx
+++ frontend/src/components/Layout/Header.tsx
@@ -4,23 +4,29 @@ import DrawerMenu from "../menu/DrawerMenu.tsx";
 import LogoutIcon from '@mui/icons-material/Logout';
 import LoginIcon from '@mui/icons-material/Login';
 import {AppUser} from "../../types/AppUser.ts";
+import ThemeToggle from "./ThemeToggle.tsx";
 
 type HeaderProps = {
 	logout: () => void;
 	appUser: AppUser | null;
 };
 export default function Header({logout, appUser}: HeaderProps) {
-
 	return (
-		<StyledHeader>
-			<DrawerMenu/>
-			<h1>CommunityCore</h1>
-			{appUser?.role &&
-				<StyledLink to={"/"} onClick={logout}><LogoutIcon fontSize="large"/><br/>Ausloggen<br/>Eingeloggt
-					als: {appUser.username}</StyledLink>
-			}
-			{!appUser?.role && <StyledLink to={"/"}><LoginIcon fontSize="large"/><br/> Login</StyledLink>}
-		</StyledHeader>
+		<>
+			<StyledHeader>
+				<DrawerMenu/>
+				<h1>CommunityCore</h1>
+				{appUser?.role &&
+					<StyledLink to={"/"} onClick={logout}><LogoutIcon fontSize="large"/><br/>Ausloggen<br/>Eingeloggt
+						als: {appUser.username}</StyledLink>
+				}
+				{!appUser?.role && <StyledLink to={"/"}><LoginIcon fontSize="large"/><br/> Login</StyledLink>}
+
+			</StyledHeader>
+			<ThemeSwich>
+				<ThemeToggle/>
+			</ThemeSwich>
+		</>
 	);
 }
 // Styles
@@ -30,18 +36,17 @@ const StyledHeader = styled.header`
     padding: 0;
     text-align: center;
     width: 100vw;
-    position: absolute;
+    position: fixed;
     top: 0;
     left: 0;
     z-index: 100;
     box-shadow: 1rem 2rem 1rem rgba(0, 0, 0, 0.2);
-    margin: 0;
     display: flex;
     justify-content: space-around;
     align-items: center;
 `;
 const StyledLink = styled(Link)`
-    color: white;
+    color: var(--text-color-layout);
     text-decoration: none;
     padding: 0.6rem;
     cursor: pointer;
@@ -49,6 +54,12 @@ const StyledLink = styled(Link)`
 
     &:hover {
         background-color: var(--hover-color);
-	    color: var(--text-color);
+        color: var(--text-color);
     }
+`;
+const ThemeSwich = styled.div`
+    position: fixed;
+    top: 2rem;
+    right: 2rem;
+    z-index: 101;
 `;
--- frontend/src/components/cashJournal/BookingTable.tsx
+++ frontend/src/components/cashJournal/BookingTable.tsx
@@ -3,6 +3,7 @@ import {DataGrid, GridColDef, GridRenderCellParams} from "@mui/x-data-grid";
 import axios from "axios";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 import EditNoteIcon from "@mui/icons-material/EditNote";
+import {useNavigate} from "react-router-dom";
 
 type IncomeProps = {
 	cashData: Booking[];
@@ -10,6 +11,7 @@ type IncomeProps = {
 	fetchCashData: () => void;
 }
 export default function BookingTable({cashData, type, fetchCashData}: IncomeProps) {
+	const navigate = useNavigate();
 	const deleteBooking = (id: string) => {
 		axios.delete(`/api/cash-journal/${id}`)
 			.then(response => {
@@ -21,10 +23,16 @@ export default function BookingTable({cashData, type, fetchCashData}: IncomeProp
 			});
 	}
 	const columns: GridColDef[] = [
-		{field: 'type', headerClassName: 'MuiDataGrid-columnHeaders',headerName: 'Buchungstyp', width: 150,renderCell: (params: GridRenderCellParams) => params.row.type === 'INCOME' ? 'Einnahme' : 'Ausgabe'},
-		{field: 'description', headerClassName: 'MuiDataGrid-columnHeaders',headerName: 'Buchungstext', width: 250},
 		{
-			field: 'category', headerClassName: 'MuiDataGrid-columnHeaders',headerName: 'Kategorie', width: 250,
+			field: 'type',
+			headerClassName: 'MuiDataGrid-columnHeaders',
+			headerName: 'Buchungstyp',
+			width: 150,
+			renderCell: (params: GridRenderCellParams) => params.row.type === 'INCOME' ? 'Einnahme' : 'Ausgabe'
+		},
+		{field: 'description', headerClassName: 'MuiDataGrid-columnHeaders', headerName: 'Buchungstext', width: 250},
+		{
+			field: 'category', headerClassName: 'MuiDataGrid-columnHeaders', headerName: 'Kategorie', width: 250,
 		},
 		{
 			field: 'date',
@@ -43,32 +51,37 @@ export default function BookingTable({cashData, type, fetchCashData}: IncomeProp
 		{
 			field: 'edit',
 			headerClassName: 'MuiDataGrid-columnHeaders',
-			headerName: 'Bearbeiten',
-			width: 150,
+			headerName: " ",
+			width: 60,
 			renderCell: (params: GridRenderCellParams) => <button
-				onClick={() => window.location.href = `/cash-journal/update/${params.row.id}`}><EditNoteIcon fontSize="large"/></button>
+				style={{backgroundColor: 'transparent', border: 'none'}}
+				onClick={() => navigate(`/cash-journal/update/${params.row.id}`)}><EditNoteIcon fontSize="large"
+			                                                                                    className="IconsBookingTable"/>
+			</button>
 		},
 		{
 			field: 'delete',
 			headerClassName: 'MuiDataGrid-columnHeaders',
-			headerName: 'Löschen',
-			width: 150,
+			headerName: ' ',
+			width: 60,
 			renderCell: (params: GridRenderCellParams) => <button
-				onClick={() => deleteBooking(params.row.id)}><DeleteForeverIcon fontSize="large"/></button>
-		},
+				style={{backgroundColor: 'transparent', border: 'none'}}
+				onClick={() => deleteBooking(params.row.id)}><DeleteForeverIcon className="IconsBookingTable"
+			                                                                    fontSize="large"/></button>
+		}
 	];
 	return (
 		<div>
 			<h3>{type}</h3>
 			<DataGrid className="custom-header" columns={columns} rows={cashData} getRowId={(row) => row.id}
-			                initialState={{
-				                pagination: {
-					                paginationModel: {
-						                pageSize: 10,
-					                },
-				                },
-			                }}
-			                sx={{fontSize: '1.4rem', color:'var(--text-color)', borderColor: 'var(--box-border-color)'}}/>
+			          initialState={{
+				          pagination: {
+					          paginationModel: {
+						          pageSize: 10,
+					          },
+				          },
+			          }}
+			          sx={{fontSize: '1.4rem', color: 'var(--text-color)', borderColor: 'var(--box-border-color)'}}/>
 		</div>
 	)
 }
